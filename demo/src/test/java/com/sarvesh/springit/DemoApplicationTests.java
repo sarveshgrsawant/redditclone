6c1e2624257003bb7276abefb34584acd917c0cd
Sarvesh <Sarvesh@DESKTOP-DB2LH0O>
2020-02-22 19:00:40 +0530

    Initial Commit for spring boot reddit clone

---
+++ demo/src/test/java/com/sarvesh/springit/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarvesh.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
